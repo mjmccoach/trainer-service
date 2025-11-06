3b372dcc6efde9d0168fbddf71abf51cd96e748f
Matthew <m.mccoach88@gmail.com>
2025-11-06 16:24:13 +0000

    MMD-13 : Add serial version uid to validation exception.

--- src/main/java/com/trainer_service/trainer_service/error/ValidationException.java
+++ src/main/java/com/trainer_service/trainer_service/error/ValidationException.java
@@ -1,6 +1,8 @@
 package com.trainer_service.trainer_service.error;
 
 public class ValidationException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public ValidationException(String message) {
         super(message);
     }
