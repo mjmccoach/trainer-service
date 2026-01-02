99f6b28f51944ccd9c86c24925df8661fcc42370
Matthew <m.mccoach88@gmail.com>
2026-01-02 15:21:44 +0000

    MMD-80 : MINOR : Add GymBadge object. Add BadgeType enum. Add BadgeResource.

---
+++ src/main/java/com/trainer_service/trainer_service/rest/BadgeResource.java
@@ -0,0 +1,18 @@
+package com.trainer_service.trainer_service.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/badge")
+@RestController
+public class BadgeResource {
+    //get by id
+    //get all
+    //create
+    //delete
+    //update
+    @GetMapping
+
+
+}
