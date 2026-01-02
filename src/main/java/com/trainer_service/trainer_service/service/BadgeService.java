b8591f08d995c5c991494f8f0efcc963cf44c914
Matthew <m.mccoach88@gmail.com>
2026-01-02 16:20:11 +0000

    MMD-82 : MINOR : Add basic crud to service from DAO.

--- src/main/java/com/trainer_service/trainer_service/service/BadgeService.java
+++ src/main/java/com/trainer_service/trainer_service/service/BadgeService.java
@@ -2,6 +2,7 @@ package com.trainer_service.trainer_service.service;
 
 import com.trainer_service.trainer_service.enums.BadgeType;
 import com.trainer_service.trainer_service.objects.GymBadge;
+import com.trainer_service.trainer_service.persistence.BadgeDAO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,18 +13,22 @@ import java.util.List;
 @AllArgsConstructor
 public class BadgeService {
 
+    private BadgeDAO badgeDAO;
+
     public GymBadge getById(int id) {
-        return new GymBadge(1, BadgeType.BOULDER);
+        return badgeDAO.getBadgeById(id);
     }
 
     public List<GymBadge> getAllGymBadges() {
-        return Collections.singletonList(new GymBadge(1, BadgeType.BOULDER));
+        return badgeDAO.getAllGymBadges();
     }
 
-    public void createGymBadge() {
+    public void createGymBadge(String name) {
+        badgeDAO.createGymBadge(name);
     }
 
     public void deleteGymBadge(int id) {
+        badgeDAO.deleteGymBadge(id);
     }
 
     //TODO Add payload and update endpoint
