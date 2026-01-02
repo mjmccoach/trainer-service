88b789cf5985ed2fcad26867c6bb8472537899c3
Matthew <m.mccoach88@gmail.com>
2026-01-02 16:27:56 +0000

    MMD-82 : MINOR : Annotate rowmapper with component. Add name input to BadgeResource on create endpoint.

--- src/main/java/com/trainer_service/trainer_service/persistence/BadgeRowMapper.java
+++ src/main/java/com/trainer_service/trainer_service/persistence/BadgeRowMapper.java
@@ -3,10 +3,12 @@ package com.trainer_service.trainer_service.persistence;
 import com.trainer_service.trainer_service.enums.BadgeType;
 import com.trainer_service.trainer_service.objects.GymBadge;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Component
 public class BadgeRowMapper implements RowMapper<GymBadge> {
     @Override
     public GymBadge mapRow(ResultSet rs, int rowNum) throws SQLException {
